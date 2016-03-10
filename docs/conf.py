c21567aa2aeacdd80babf4d05402e269811a206b
mvousden <mark.vousden@soton.ac.uk>
2016-03-10 16:37:41 +0000

    DOC-026: Add sphinx theme for HTML docs.

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,12 @@
 import sys
 import os
 
+try:
+    import sphinx_rtd_theme
+except ImportError as e:
+    print "Could not import sphinx_rtd_theme. Please install using `sudo pip install sphinx_rtd_theme`."
+    raise
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -107,7 +113,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
